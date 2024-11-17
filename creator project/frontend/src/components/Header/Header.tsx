2b284a46ec7b9d9d8f1a26b3415316a42a22fb81
maniaghili <aligholimani@gmail.com>
2024-11-17 15:57:35 +0330

    update project

--- creator project/frontend/src/components/Header/Header.tsx
+++ creator project/frontend/src/components/Header/Header.tsx
@@ -20,7 +20,7 @@ const [colaps,setColaps] = useState(false)
              <div className='h-[1px] w-[50%] bg-black'></div>
              <div className='h-[1px] w-[50%] bg-black'></div>
           </button>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-blue-700 sm:hidden lg:inline">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-blue-700 ">
             <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z"></path>
             <path fill-rule="evenodd" d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z" clip-rule="evenodd"></path>
         </svg>
@@ -83,11 +83,27 @@ const [colaps,setColaps] = useState(false)
       </div>
      </div>
      {
-      colaps?<div className='active_colaps z-50 absolute  h-screen w-1/2 lg:hidden'>
-      <p onClick={()=>{setColaps(false)}} className='m'>X</p>
+      colaps?<div  className=' active_colaps z-40 absolute  h-screen w-full lg:hidden flex justify-between'>
+       <div className='z-50 w-4/5 flex justify-between bg-neutral-100'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-blue-700 sm:hidden lg:inline">
+            <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z"></path>
+            <path fill-rule="evenodd" d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z" clip-rule="evenodd"></path>
+        </svg>
+        <div className='flex flex-col gap-0'>
+          <p className="opacity-70 akademi-header font-semibold ">آکادمی</p>
+          <p className=" na-header  font-bold text-blue-700">
+            نابغه
+          </p>
+        </div>
+        <p onClick={()=>{setColaps(false)}}>X
+
+      </p>
+      </div>
+      <div onClick={()=>{setColaps(false)}} className='opacity-10 w-1/2'></div>
+     
      </div>:
      <div className=' colaps z-50 absolute  h-screen w-1/2 lg:hidden'>
-     <p onClick={()=>{setColaps(false)}} className='m'>X</p>
+     
     </div>
      }
     </div>
