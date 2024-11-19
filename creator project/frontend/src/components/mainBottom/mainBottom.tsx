01f6d9cccdd9320b28a8539853caead467022834
maniaghili <aligholimani@gmail.com>
2024-11-19 23:35:59 +0330

    create mainBottom

--- creator project/frontend/src/components/mainBottom/mainBottom.tsx
+++ creator project/frontend/src/components/mainBottom/mainBottom.tsx
@@ -1,4 +1,5 @@
 import './mainBottom.css'
+import Maincomments from '../maincomments/maincomments'
 const MainBottom = () => {
   return (
     <div className="w-full flex justify-center items-center ">
@@ -17,24 +18,7 @@ const MainBottom = () => {
            </div>
         </div>
         <div className="ssm:w-full lg:w-1/2 h-full flex flex-col justify-center">
-        <div className=" rounded-3xl border border-zinc-300 flex flex-col items-center">
-            <div className="opacity-70 w-5/6 my-3">
-                من به تیم شما بابت پشتیبانی عالیتان از وبسایتتان تشکر می‌کنم. سوالات
-                و
-                مشکلات من به سرعت پاسخ داده می‌شوند و همیشه یک راه حل مناسب برای هر
-                مشکل
-                پیدا می‌کنید. این امر بسیار قابل ارزش است.
-            </div>
-            <div className="flex items-center my-5">
-                <img src="../../../public/01.jpeg" className="w-10 h-10 rounded-full" alt="" />
-                <div>
-                    <p>جلال بهرامی راد</p>
-                    <span className="opacity-70">
-                                                            دوره پروژه محور React و Next
-                    </span>
-                </div>
-            </div>
-        </div>
+        <Maincomments />
         </div>
       </div>
     </div>
---
+++ creator project/frontend/src/components/maincomments/maincomments.tsx
@@ -0,0 +1,25 @@
+
+const maincomments = () => {
+  return (
+    <div className=" rounded-3xl border border-zinc-300 flex flex-col items-center">
+    <div className="opacity-70 w-5/6 my-3">
+        من به تیم شما بابت پشتیبانی عالیتان از وبسایتتان تشکر می‌کنم. سوالات
+        و
+        مشکلات من به سرعت پاسخ داده می‌شوند و همیشه یک راه حل مناسب برای هر
+        مشکل
+        پیدا می‌کنید. این امر بسیار قابل ارزش است.
+    </div>
+    <div className="flex items-center my-5">
+        <img src="../../../public/01.jpeg" className="w-10 h-10 rounded-full" alt="" />
+        <div>
+            <p>جلال بهرامی راد</p>
+            <span className="opacity-70">
+                                                    دوره پروژه محور React و Next
+            </span>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default maincomments
