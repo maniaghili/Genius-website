73d6549576715e1109a4430de85db9fa724ed8a5
maniaghili <aligholimani@gmail.com>
2024-11-22 00:03:11 +0330

    update index

--- creator project/frontend/src/components/HeaderLi/HeaderLi.tsx
+++ creator project/frontend/src/components/HeaderLi/HeaderLi.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useRef, useState } from "react"
+import { PropsWithChildren, useState } from "react"
 import './HeaderLi.css'
 type pro = {
     svg:boolean
@@ -11,23 +11,10 @@ const HeaderLi:React.FC<PropsWithChildren<pro>> = ({svg,children}) => {
 
   const [ali,setAli] = useState(false)
   
-type rr = {
-    current:any
-}
-    
-   const hover = useRef() as rr
-   
-   const hoverHandler = () => {
-    hover.current.classList.add('active')
-    
-    }
 
-    const endHandler = () => {
-      hover.current.classList.remove('active')
-    }
 
   return (
-    <li  onMouseMove={hoverHandler} onMouseLeave={endHandler} className='relative  h-14 flex items-center gap-1'>
+    <li className=' group relative  h-14 flex items-center gap-1'>
       <div className="flex justify-end jjl"><p className=" opacity-80" >{children}</p></div>
       {
         svg&&
@@ -35,8 +22,8 @@ type rr = {
         <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="opacity-80 w-5 h-5 gg">
         <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"></path>
           </svg>
-          <div className="bg-white drop_down absolute z-10  top-10  w-48 min-h-40 rounded-lg  "  ref={hover} >
-              <ul onMouseLeave={endHandler} onMouseEnter={()=>{hoverHandler()}} className="w-full ">
+          <div className="bg-white drop_down absolute z-10  top-10  w-48 min-h-40 rounded-lg  group-hover:visible ">
+              <ul className="w-full ">
                 <li onMouseEnter={()=>{setAli(true)}} onMouseLeave={()=>{setAli(false)}} className="text-black mt-1 hover:text-blue-600  h-8 cursor-pointer flex items-center text-[13px] font-semibold mr-2 justify-between ">
                       <p>برنامه نویسی وب</p>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 ml-2 h-5">
