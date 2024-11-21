870f35ba83fa6667ca08bd9d1b87e30f44f902ec
maniaghili <aligholimani@gmail.com>
2024-11-21 10:20:52 +0330

    update footer bottom

--- creator project/frontend/src/components/footerBottom/footerBottom.tsx
+++ creator project/frontend/src/components/footerBottom/footerBottom.tsx
@@ -14,8 +14,8 @@ const footerBottom = () => {
                 </svg>
                 </button>
             </div>
-            <div className="w-full mt-10 flex items-center">
-                <div className="w-lg-[20%] w-md-[45%] flex items-center gap-2">
+            <div className="w-full mt-10 flex ssm:flex-col md:flex-row lg:items-center">
+                <div className="md:w-1/5 ssm:w-full flex items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-blue-700 ">
                         <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z"></path>
                         <path fill-rule="evenodd" d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z" clip-rule="evenodd"></path>
@@ -29,8 +29,8 @@ const footerBottom = () => {
          
                 </div> 
 
-               <div className="w-4/5 flex justify-center gap-8 ">
-                <div className="flex items-center gap-4 ">
+               <div className="lg:w-4/5 ssm:w-full flex lg:justify-center ssm:justify-start ssm:mt-3 gap-8 ">
+                 <div className="flex items-center gap-4 ">
                     <CircleButton>
                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                         <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 0 1 3.5 2h1.148a1.5 1.5 0 0 1 1.465 1.175l.716 3.223a1.5 1.5 0 0 1-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 0 0 6.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 0 1 1.767-1.052l3.223.716A1.5 1.5 0 0 1 18 15.352V16.5a1.5 1.5 0 0 1-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 0 1 2.43 8.326 13.019 13.019 0 0 1 2 5V3.5Z" clip-rule="evenodd"></path>
@@ -57,7 +57,7 @@ const footerBottom = () => {
             </div>
 
             <div className="w-full  mt-5 flex justify-start">
-                <div className="bg-slate-100 p-5 rounded-3xl lg:w-5/12 flex justify-center items-center flex-col gap-2">
+                <div className="bg-slate-100 p-5 rounded-3xl h-fit lg:w-5/12 flex justify-center items-center flex-col gap-2">
                    <div className="w-11/12 ">
                       <p className="font-bold w-full">درباره ما</p>
                       <span className="opacity-70">
@@ -65,7 +65,7 @@ const footerBottom = () => {
                       </span>
                    </div>
                 </div>
-                <div className=" mr-10 ml-5 flex flex-col gap-3">
+                <div className="flex flex-col gap-3 lg:w-1/3 items-center">
                     <span className="font-bold">لینک های مفید</span>
                     <ul className="flex flex-col gap-1">
                         <li className="opacity-70 text-[12px] font-bold"><a href="">قوانین و مقررات</a></li>
@@ -74,7 +74,7 @@ const footerBottom = () => {
                         <li className="opacity-70 text-[12px] font-bold"><a href="">ارتباط با ما</a></li>
                     </ul>
                 </div>
-                <div className="mr-12 flex flex-col gap-5">
+                <div className="flex flex-col gap-5">
                    <p className="font-bold">خبرنامه</p>
                    <span>برای اطلاع از جدیدترین اخبار و جشنوراه‌های تخفیفی نابغه ایمیل خود را وارد کنید.</span>
                    <div className=" rounded-2xl flex items-center gap-2">
@@ -89,10 +89,36 @@ const footerBottom = () => {
                        </div>
                        <div className=" flex justify-end">
                            
-                           <button className="bg-blue-600 text-white hover:bg-blue-500 transition-all rounded-xl px-5"> 
-                              <p className="w-full font-black ">ثبت ایمیل</p>
+                           <button className="bg-blue-600 text-white font-black hover:bg-blue-500 transition-all rounded-xl w-20 py-2"> 
+                              ثبت ایمیل
                           </button>
                        </div>
+                   </div>
+                   <div className="flex flex-col gap-2 mb-5">
+                    <span className="font-extrabold">شبکه های اجتماعی</span>
+                    <div className="flex gap-3">
+                        <CircleButton>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-5 h-5">
+                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                           <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
+                        </svg>
+                        </CircleButton>
+                        <CircleButton>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-5 h-5">
+                            <path d="m22 2-7 20-4-9-9-4Z"></path>
+                            <path d="M22 2 11 13"></path>
+                        </svg>
+                        </CircleButton>
+                        <CircleButton>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-5 h-5">
+                            <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17">
+                            </path>
+                            <path d="m10 15 5-3-5-3z"></path>
+                        </svg>
+
+                        </CircleButton>
+                    </div>
                    </div>
                 </div>
             </div>
