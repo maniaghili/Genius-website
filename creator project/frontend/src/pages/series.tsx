e92b861507e7070953b2968bd7c382475bcf5580
maniaghili <aligholimani@gmail.com>
2024-11-23 18:24:11 +0330

    update

--- creator project/frontend/src/pages/series.tsx
+++ creator project/frontend/src/pages/series.tsx
@@ -6,10 +6,11 @@ const series = () => {
     <>
     <HeaderTop /> 
     <div className="w-full  flex justify-center">
-      <div className="w-[96%] h-96 flex items-center">
-        <div className="w-1/4 h-full bg-slate-400">
-          <div className='lg:w-full flex justify-center my-5 h-full gap-3 '>
-            <div className=' flex justify-center items-center sm:mr-2 lg:mr-0'>
+      <div className="w-[96%] h-[800px] flex gap-3">
+       <div className="bg-white h-full">
+         <div className="h-fit sticky top-[20px] ">
+          <div className='lg:w-full flex justify-center my-4 gap-3 '>
+            <div className='flex justify-center items-center sm:mr-2 lg:mr-0'>
                 <div className='h-14 bg-blue-700 w-14  rounded-full flex justify-center items-center'>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-white">
                      <path fill-rule="evenodd" d="M9.664 1.319a.75.75 0 0 1 .672 0 41.059 41.059 0 0 1 8.198 5.424.75.75 0 0 1-.254 1.285 31.372 31.372 0 0 0-7.86 3.83.75.75 0 0 1-.84 0 31.508 31.508 0 0 0-2.08-1.287V9.394c0-.244.116-.463.302-.592a35.504 35.504 0 0 1 3.305-2.033.75.75 0 0 0-.714-1.319 37 37 0 0 0-3.446 2.12A2.216 2.216 0 0 0 6 9.393v.38a31.293 31.293 0 0 0-4.28-1.746.75.75 0 0 1-.254-1.285 41.059 41.059 0 0 1 8.198-5.424ZM6 11.459a29.848 29.848 0 0 0-2.455-1.158 41.029 41.029 0 0 0-.39 3.114.75.75 0 0 0 .419.74c.528.256 1.046.53 1.554.82-.21.324-.455.63-.739.914a.75.75 0 1 0 1.06 1.06c.37-.369.69-.77.96-1.193a26.61 26.61 0 0 1 3.095 2.348.75.75 0 0 0 .992 0 26.547 26.547 0 0 1 5.93-3.95.75.75 0 0 0 .42-.739 41.053 41.053 0 0 0-.39-3.114 29.925 29.925 0 0 0-5.199 2.801 2.25 2.25 0 0 1-2.514 0c-.41-.275-.826-.541-1.25-.797a6.985 6.985 0 0 1-1.084 3.45 26.503 26.503 0 0 0-1.281-.78A5.487 5.487 0 0 0 6 12v-.54Z" clip-rule="evenodd"></path>
@@ -21,7 +22,20 @@ const series = () => {
                 <p className="text-[11px] font-bold opacity-70">دوره ببین، تمرین کن، برنامه نویس شو </p>
             </div>
           </div>
+          <div className=" flex flex-col">
+            <p className="font-bold text-[13px]">جست وجو دوره</p>
+           <div className="bg-slate-200 h-10 rounded-xl flex items-center justify-between my-3">
+              <input type="text" className="bg-slate-200 h-10 w-full " placeholder="عنوان دوره..." />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="ml-3 w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"></path></svg>
+           </div>
+           <div className="bg-slate-200 h-10 flex round ed-xl items-center justify-between my-2">
+              <input type="text" className="bg-slate-200 h-10 w-full " placeholder="عنوان دوره..." />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="ml-3 w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"></path></svg>
+           </div>
+
+          </div>
         </div>
+       </div>
         <div className="w-3/4 bg-slate-600 h-full">s</div>
       </div>
     </div>
