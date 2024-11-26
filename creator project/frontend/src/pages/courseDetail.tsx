6908456503af67c15fe0344631c4340b4ce794d0
maniaghili <aligholimani@gmail.com>
2024-11-26 18:58:05 +0330

    update course page

--- creator project/frontend/src/pages/courseDetail.tsx
+++ creator project/frontend/src/pages/courseDetail.tsx
@@ -115,13 +115,23 @@ const courseDetail = () => {
               </div>
             </div>
              <CommentBox />
-             <CommentBox />
-             <CommentBox />
+             
              
            {/* comments */}
           </div>
        </div>
-       <div className="w-[35%] rounded-2xl overflow-hidden bg-red-500" > sz</div>
+       <div className="w-[35%] rounded-2xl overflow-hidden bg-red-200" > 
+          <div className="bg-slate-200 flex flex-col items-center">
+             <div className="w-11/12 bg-white gap-2 flex items-center rounded-b-2xl h-20">
+             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="mr-3 w-5 h-5">
+             <path d="M10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM6 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM1.49 15.326a.78.78 0 0 1-.358-.442 3 3 0 0 1 4.308-3.516 6.484 6.484 0 0 0-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 0 1-2.07-.655ZM16.44 15.98a4.97 4.97 0 0 0 2.07-.654.78.78 0 0 0 .357-.442 3 3 0 0 0-4.308-3.517 6.484 6.484 0 0 1 1.907 3.96 2.32 2.32 0 0 1-.026.654ZM18 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM5.304 16.19a.844.844 0 0 1-.277-.71 5 5 0 0 1 9.947 0 .843.843 0 0 1-.277.71A6.975 6.975 0 0 1 10 18a6.974 6.974 0 0 1-4.696-1.81Z">
+              </path>
+              </svg>
+
+             <p className="font-bold  text-[14px]">نام نویسی در دوره</p>
+             </div>
+          </div>
+       </div>
       </div>
     </div>
     <FooterBottom />
