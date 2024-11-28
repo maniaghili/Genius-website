ce540ebadc64ac6c2325dbc57f4adb79ae5cba4b
maniaghili <aligholimani@gmail.com>
2024-11-28 08:44:08 +0330

    update course page

--- creator project/frontend/src/components/footerBottom/footerBottom.tsx
+++ creator project/frontend/src/components/footerBottom/footerBottom.tsx
@@ -97,20 +97,20 @@ const footerBottom = () => {
                    <div className="flex flex-col gap-2 mb-5">
                     <span className="font-extrabold">شبکه های اجتماعی</span>
                     <div className="flex gap-3">
-                        <CircleButton>
+                        <CircleButton id={undefined}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-5 h-5">
                             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                             <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
                            <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
                         </svg>
                         </CircleButton>
-                        <CircleButton>
+                        <CircleButton id={undefined}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-5 h-5">
                             <path d="m22 2-7 20-4-9-9-4Z"></path>
                             <path d="M22 2 11 13"></path>
                         </svg>
                         </CircleButton>
-                        <CircleButton>
+                        <CircleButton id={undefined}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-5 h-5">
                             <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17">
                             </path>
