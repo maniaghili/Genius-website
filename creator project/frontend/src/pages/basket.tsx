87e15bd8ff21339ea7c8e338ef6d3c840b417fab
maniaghili <aligholimani@gmail.com>
2024-11-28 20:19:10 +0330

    create basket

---
+++ creator project/frontend/src/pages/basket.tsx
@@ -0,0 +1,14 @@
+
+import HeaderTop from "../components/HeaderTop/HeaderTop";
+import FooterBottom from "../components/footerBottom/footerBottom";
+const basket = () => {
+  return (
+    <>
+    <HeaderTop />
+    <div>salam</div>
+    <FooterBottom />
+    </>
+  )
+}
+
+export default basket
--- creator project/frontend/src/router/router.tsx
+++ creator project/frontend/src/router/router.tsx
@@ -4,6 +4,7 @@ import Register from "../pages/register";
 import Series from "../pages/series";
 import CourseDetail from "../pages/courseDetail";
 import UserProfile from "../pages/userprofile";
+import Basket from "../pages/basket";
 type ss = {
     path:string ,element: JSX.Element
 }
@@ -14,7 +15,8 @@ export const Router:ss[] = [
     {path:'/register' ,element:<Register />},
     {path:'/series' ,element:<Series />},
     {path:'/courseDetail/:courseName',element:<CourseDetail />},
-    {path:'/userprofile',element:<UserProfile />}
+    {path:'/userprofile',element:<UserProfile />},
+    {path:'/userbasket',element:<Basket />}
 ]
 
  
