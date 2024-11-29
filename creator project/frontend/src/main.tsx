e77446fa14bff8033bb9f7053ef77b800566e6a0
maniaghili <aligholimani@gmail.com>
2024-11-29 11:10:41 +0330

    update project

--- creator project/frontend/src/main.tsx
+++ creator project/frontend/src/main.tsx
@@ -1,6 +1,9 @@
 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import './fonts/Mj_Mokhtar.ttf'
+import './fonts/Mj_Free_0.ttf'
+import './fonts/Samim.ttf'
 import App from './App.tsx'
 import './index.css'
 
