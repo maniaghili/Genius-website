67864c9c2231cc4040daa5e6c71242a68c1c1f13
maniaghili <aligholimani@gmail.com>
2024-12-12 18:30:39 +0330

    update  project colors

--- creator project/frontend/src/pages/register/register.tsx
+++ creator project/frontend/src/pages/register/register.tsx
@@ -53,8 +53,8 @@ const register:FC =memo(() => {
     <HeaderTop />
   <div className="h-screen flex items-start justify-center bg-background p-5">
       <div className="w-full max-w-sm space-y-5  rounded-2xl">
-          <div className="bg-gradient-to-b from-secondary bg-gray-100 to-background rounded-3xl space-y-5 px-5 pb-5">
-              <div  className="bg-white rounded-b-3xl space-y-2 p-5">
+          <div className="bg-gradient-to-b from-secondary bg-gray-100 dark:text-gray-300 dark:bg-gray-900 to-background rounded-3xl space-y-5 px-5 pb-5">
+              <div  className="bg-white dark:bg-gray-800 rounded-b-3xl space-y-2 p-5">
                   <a href="./home.html" className="inline-flex items-center gap-2 text-primary">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-blue-600">
                           <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z"></path>
@@ -77,15 +77,15 @@ const register:FC =memo(() => {
 
                   {/* <!-- form:field:wrapper --> */}
                   <div className="flex items-start relative flex-col gap-3">
-                      <input type="text" {...register('userName',{required:'نام کاربری را وارد کنید'})} placeholder="نام کاربری خود را وارد نمایید" dir="ltr" className="bg-gray-50 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" />
+                      <input type="text" {...register('userName',{required:'نام کاربری را وارد کنید'})} placeholder="نام کاربری خود را وارد نمایید" dir="ltr" className="bg-gray-50 dark:bg-gray-800 dark:border-gray-700 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" />
                       {errors.userName && <span className="text-[9px] w-fit font-bold text-red-600">{errors.userName.message  as ReactNode}</span>}
-                      <input type="text" {...register('name',{required:'نام خود را وارد نمایید'})} placeholder="نام خود را وارد نمایید" dir="ltr" className="bg-gray-50 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" /> 
+                      <input type="text" {...register('name',{required:'نام خود را وارد نمایید'})} placeholder="نام خود را وارد نمایید" dir="ltr" className="bg-gray-50 dark:bg-gray-800 dark:border-gray-700 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" /> 
                       {errors.name && <span className="text-[9px] w-fit font-bold text-red-600">{errors.name.message as ReactNode}</span>}
-                      <input type="text" {...register('password',{minLength:{value:8,message:'8 رقم الزامیست'}})} placeholder="رمز عبور خود را وارد نمایید" dir="ltr" className="bg-gray-50 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" />
+                      <input type="text" {...register('password',{minLength:{value:8,message:'8 رقم الزامیست'}})} placeholder="رمز عبور خود را وارد نمایید" dir="ltr" className="bg-gray-50 dark:bg-gray-800 dark:border-gray-700 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" />
                       {errors.password && <span className="text-[9px] w-fit font-bold text-red-600">{errors.password.message  as ReactNode}</span>}
-                      <input type="text" {...register('phone',{required:'شماره تماس را وارد کنید'})} placeholder="شماره تماس خود ر وارد نمایید" dir="ltr" className="bg-gray-50 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" />
+                      <input type="text" {...register('phone',{required:'شماره تماس را وارد کنید'})} placeholder="شماره تماس خود ر وارد نمایید" dir="ltr" className="bg-gray-50 dark:bg-gray-800 dark:border-gray-700 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" />
                       {errors.phone && <span className="text-[9px] w-fit font-bold text-red-600">{errors.phone.message  as ReactNode}</span>}
-                      <input type="text" {...register('email',{required:'وارد کردن ایمیل الزامی است'})} placeholder="ایمیل خود را وارد نمایید" dir="ltr" className="bg-gray-50 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" />
+                      <input type="text" {...register('email',{required:'وارد کردن ایمیل الزامی است'})} placeholder="ایمیل خود را وارد نمایید" dir="ltr" className="bg-gray-50 dark:bg-gray-800 dark:border-gray-700 border form-input w-full h-11 !ring-0 !ring-offset-0 bg-secondary border-border focus:border-border rounded-xl text-sm text-foreground placeholder:text-right px-5" />
                       {errors.email && <span className="text-[9px] w-fit font-bold text-red-600">{errors.email.message  as ReactNode}</span>}
                       
                   </div>
@@ -102,7 +102,7 @@ const register:FC =memo(() => {
               </form>
               {/* <!-- end auth:verification:form --> */}
           </div>
-          <div className="bg-gray-100 rounded-xl space-y-5 p-5">
+          <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-300 rounded-xl space-y-5 p-5">
               <div className="font-medium text-xs text-center text-muted">
                   ورود شما به معنای پذیرش <a href="#" className="text-foreground transition-colors hover:text-primary hover:underline">شرایط</a> و
                   <a href="#" className="text-foreground transition-colors hover:text-primary hover:underline">قوانین
