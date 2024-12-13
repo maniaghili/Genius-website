1845d14dc5daf784e446f9f5f5439a6aa14e5b03
maniaghili <aligholimani@gmail.com>
2024-12-13 14:33:03 +0330

    update  project colors

--- creator project/frontend/src/components/isNotData/isNotData.tsx
+++ creator project/frontend/src/components/isNotData/isNotData.tsx
@@ -3,8 +3,11 @@ const isNotData = (dataName:{
     dataName:string
 }) => {
   return (
-    <div className="bg-red-500 text-white flex justify-center items-center rounded-2xl my-5 w-full h-10"><p>
-      {dataName.dataName}</p></div>
+    <div className="bg-red-500 text-white flex justify-center items-center rounded-2xl my-5 w-full h-10">
+      <p >
+        {dataName.dataName}
+      </p>
+    </div>
   )
 }
 
