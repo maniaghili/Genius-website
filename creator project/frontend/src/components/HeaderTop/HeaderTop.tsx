25a5a7437601e7b32eb138b104673ff0aa6e5b9e
maniaghili <aligholimani@gmail.com>
2024-12-13 13:37:31 +0330

    update  project colors

--- creator project/frontend/src/components/HeaderTop/HeaderTop.tsx
+++ creator project/frontend/src/components/HeaderTop/HeaderTop.tsx
@@ -81,12 +81,12 @@ const HeaderTop = memo(() => {
           </button>
           </CircleButton>
           </div>
-          <CircleButton >
-          <Link className='parent dark:bg-gray-900 dark:text-gray-200 bg-slate-100 hover:bg-slate-200 transition-all' to={'/userbasket'}>
+          <CircleButton id={1}>
+          <button className='parent bg-slate-100 dark:text-gray-200 dark:bg-gray-900 hover:bg-slate-200 transition-all '>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"></path>
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"></path>
           </svg>
-         </Link>
+          </button>
           </CircleButton>
           <CircleButton>
           
